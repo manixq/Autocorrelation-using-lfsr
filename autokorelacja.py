2e3a2fb9c3aa1151bd7e3ada6b674232dc9783a7
Pawel M <obramanix@gmail.com>
2017-03-23 21:54:41 +0100

    - added autocorrelate function

--- autokorelacja.py
+++ autokorelacja.py
@@ -1,3 +1,6 @@
+import sys
+print sys.argv
+
 #base_ format: XXXX
 #example: 0001
 
@@ -7,20 +10,36 @@
 
 def lfsr(base_, polynomial_):
 	current = base_
-	nbits = base_[0]
-	result = [0] * len(base_)
+	result = []
+	result_ = []
 	while 1:
+		result_.append(current[-1])
 		xor = current[len(polynomial_) - polynomial_[1]]
-		result[0] = xor ^ current[-1]
+		result.append(xor ^ current[-1])
 		for i in range(len(base_) - 1):
-			result[i + 1] = current[i]
+			result.append(current[i])
 		current = []
 		for x in result:
 			current.append(x)
+		result = []
 		print current
 		print "\n" 
 		if current == base_:
 			break
+	return result_
+
+def autocorrelate(Rxx, n):
+	for i in range(len(Rxx)):
+		if Rxx[i] == 0:
+			Rxx[i] = -1
+	Rxx_n = Rxx[n:] + Rxx[:n]
+	print Rxx
+	print Rxx_n
+	result = [c_i * c_n for c_i, c_n in zip(Rxx, Rxx_n)]
+	print result
+	result = sum(result)
+	print result
 
 
-lfsr([0, 0, 0, 1], [4,3,1])
+result = lfsr([0, 0, 0, 1], [4,3,1])
+autocorrelate(result, 15)
